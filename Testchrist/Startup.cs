2ba7d03d90380e22d839fc70f8255570d9f3168e
Алексей <swtorigra@gmail.com>
2015-09-29 17:36:16 +0300

    2231321

---
+++ Testchrist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testchrist.Startup))]
+namespace Testchrist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
